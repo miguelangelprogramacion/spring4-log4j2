2294fb4a7cb809908c4fec3e3b266510bcaf95f7
Miguel Ángel Fernández Fernández <miguelangelprogramacion@gmail.com>
2015-09-10 09:33:59 +0200

    maven project struture. Also, .gitignore modificated to ignore eclipse features

---
+++ src/main/java/world/we/deserve/App.java
@@ -0,0 +1,13 @@
+package world.we.deserve;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
